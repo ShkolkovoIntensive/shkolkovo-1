ba01db64bd5bcd11ece63b83d8a7b5cde61c9e12
Михаил Кизимов <mi.kizimov@gmail.com>
2020-11-21 21:17:31 +0300

    Второй commit

--- Zadacha1/Program.cs
+++ Zadacha1/Program.cs
@@ -8,7 +8,8 @@ namespace shkolkovo
         {
             Console.Write("Write your name: ");             
             string x = Console.ReadLine();
-            Console.WriteLine("Hello, {0}", x);
+            Console.WriteLine("Hello, k{0}", x);
+
         }
     }
 }
